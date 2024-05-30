a17a2f3f734b50e9afbfdcae4950d111da466524
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-05-30 12:27:54 +0200

    End of the first version

--- pizza_mama/Pages/Admin/Index.cshtml.cs
+++ pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -6,18 +6,34 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace pizza_mama.Pages.Admin
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public bool displayInvalidAccountMessage = false;
+        IConfiguration configuration;
+        public IndexModel(IConfiguration configuration)
         {
+            this.configuration = configuration;
+        }
+        public IActionResult OnGet(string ReturnUrl = "/admin/pizzas")
+        {
+            if (this.HttpContext.User.Identity.IsAuthenticated)
+                return Redirect(ReturnUrl);
+
+            return Page();
         }
 
-        public async Task<IActionResult> OnPost(string username, string password, string ReturnUrl) 
+        public async Task<IActionResult> OnPostAsync(string username, string password, string ReturnUrl) 
         {
-            if (username == "admin")
+            var authSection = configuration.GetSection("Auth");
+
+            string adminLogin = authSection["AdminLogin"];
+            string adminPassword = authSection["AdminPassword"];
+
+            if (username == adminLogin && password == adminPassword)
             {
                 var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
                 var claimsIdentity = new ClaimsIdentity(claims, "Login");
@@ -25,9 +41,17 @@ namespace pizza_mama.Pages.Admin
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, 
                     new ClaimsPrincipal(claimsIdentity));
 
+                this.displayInvalidAccountMessage = false;
                 return Redirect(ReturnUrl == null ? "/Admin/Pizzas" : ReturnUrl);
             }
+            this.displayInvalidAccountMessage = true;
             return Page();
+        }
+
+        public async Task<IActionResult> OnGetLogout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/admin");
         }
     }
 }
